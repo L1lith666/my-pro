f485cfbc06064b88c3ab7b813bb6fb81157d5c8d
Lida <ludka666@gmail.com>
2018-05-14 22:49:16 +0100

    Use SNS to notify when portfolio is deployed

--- upload-pro-lambda.py
+++ upload-pro-lambda.py
@@ -4,18 +4,26 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-1:211335235430:deployProTopic')
 
-    s3 = boto3.resource('s3')
+    try:
 
-    pro_bucket = s3.Bucket('pro.wolil.myinstance.com')
-    build_bucket = s3.Bucket('probuild.wolil.myinstance.com')
-    pro_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('probuild.zip', pro_zip)
+        s3 = boto3.resource('s3')
 
-    with zipfile.ZipFile(pro_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            pro_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            pro_bucket.Object(nm).Acl().put(ACL='public-read')
+        pro_bucket = s3.Bucket('pro.wolil.myinstance.com')
+        build_bucket = s3.Bucket('probuild.wolil.myinstance.com')
+        pro_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('probuild.zip', pro_zip)
+
+        with zipfile.ZipFile(pro_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                pro_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                pro_bucket.Object(nm).Acl().put(ACL='public-read')
+        topic.publish(Subject="Pro deployed", Message="Pro Deployed successfully")
+    except:
+        topic.publish(Subject="Pro deployment failed!", Message="Pro not deployed successfully")
+        raise
     return 'Hello from Lambda'
